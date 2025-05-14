f97687cf1db699a796c966d49227d5e1fa187732
acarogluege <acarogluege@gmail.com>
2025-05-14 16:25:54 +0300

    Move and update are changed so to move and turn are now seperated

--- player.py
+++ player.py
@@ -1,6 +1,7 @@
 from circleshape import CircleShape
 from constants import PLAYER_RADIUS
 from constants import PLAYER_TURN_SPEED
+from constants import PLAYER_SPEED
 import pygame
 
 
@@ -8,7 +9,7 @@ class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x,y,PLAYER_RADIUS)
         self.rotation = 0
-
+        keys = pygame.key.get_pressed()
 
     def draw(self, screen):
         pygame.draw.polygon(screen, (255,255,255), self.triangle(), width=2)
@@ -27,15 +28,23 @@ class Player(CircleShape):
 
 
     def update(self, dt):
+        self.move(dt)
         keys = pygame.key.get_pressed()
         if keys[pygame.K_a]:
             self.rotation -= PLAYER_TURN_SPEED * dt
         if keys[pygame.K_d]:
             self.rotation += PLAYER_TURN_SPEED * dt
             
+        if keys[pygame.K_w]:
+            self.move(dt)
+        if keys[pygame.K_s]:
+            self.move(-dt)
+
+    def move(self, dt):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_w]:
+            forward = pygame.Vector2(0, 1).rotate(self.rotation)
+            self.position += forward * dt * PLAYER_SPEED
         if keys[pygame.K_s]:
             forward = pygame.Vector2(0, -1).rotate(self.rotation)
-            self.position += forward * dt * PLAYER_TURN_SPEED
-        if keys[pygame.K_w]:
-            forward = pygame.Vector2(0, -1).rotate(self.rotation)
-            self.position += -forward * dt * PLAYER_TURN_SPEED
+            self.position += -forward * dt * PLAYER_SPEED
