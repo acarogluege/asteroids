613dea853c351cfd896df22e751b7573ad9e3962
acarogluege <acarogluege@gmail.com>
2025-05-14 15:45:23 +0300

    Player constant, circle_shape and player is added

--- main.py
+++ main.py
@@ -1,21 +1,30 @@
 import pygame
 from constants import *
-
+from player import Player
 
 def main():
     pygame.init()
     print("Starting Asteroids!")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    Clock = pygame.time.Clock()
+    dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill((0, 0, 0))
+
+        # Screen fill color
         fill_color = (0, 0, 0)
         screen.fill(fill_color)
-    
 
-    display.flip()
+        # Draw the player
+        player.draw(screen)
 
+        # Update display
+        pygame.display.flip()
+        dt = Clock.tick(60) / 1000
+        
 if __name__ == "__main__":
     main()
